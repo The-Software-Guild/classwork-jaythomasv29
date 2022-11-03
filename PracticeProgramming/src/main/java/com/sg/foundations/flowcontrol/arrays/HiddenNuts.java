35d304f28e607af56da45b557de1288f0e997a5f
jamesvongampai <jaythomasv29@gmail.com>
2022-11-03 11:43:17 -0700

    add Factorizer, InterestCalculator, LuckySevens

--- PracticeProgramming/src/main/java/com/sg/foundations/flowcontrol/arrays/HiddenNuts.java
+++ PracticeProgramming/src/main/java/com/sg/foundations/flowcontrol/arrays/HiddenNuts.java
@@ -14,15 +14,19 @@ public class HiddenNuts {
     int start = 0;
     int end = hidingSpots.length - 1;
     while(start != end) {
-      if(hidingSpots[start] != null && hidingSpots[start].equals("Nut")){
-        System.out.println("Found it! It's in spot# " + start);
-        return;
-      } else if(hidingSpots[end] != null && hidingSpots[end].equals("Nut")) {
-        System.out.println("Found it! It's in spot# " + end);
-        return;
-      }
+     if(checkHidingSpots(hidingSpots, start, end)) return;
       start++;
       end--;
     }
+  }
+  public static boolean checkHidingSpots(String[] hidingSpots, int start, int end) {
+    if(hidingSpots[start] != null && hidingSpots[start].equals("Nut")){
+      System.out.println("Found it! It's in spot# " + start);
+      return true;
+    } else if(hidingSpots[end] != null && hidingSpots[end].equals("Nut")) {
+      System.out.println("Found it! It's in spot# " + end);
+      return true;
+    }
+    return false;
   }
 }
